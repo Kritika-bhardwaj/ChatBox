4f1632db5a511ba1a8052b07c1d004d96746411e
charu07goel <charu07goel@gmail.com>
2018-03-14 22:42:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/reshugoel/chatbox/UserDetails.java
@@ -0,0 +1,11 @@
+package com.example.reshugoel.chatbox;
+
+/**
+ * Created by Reshu Goel on 2/8/2018.
+ */
+
+public class UserDetails {
+     static String username="";
+     static String password="";
+    static String chatWith="";
+}
